fdf6392863ee0c51977e059da3543b64829f568a
Grant <granttlalonde@gmail.com>
2019-10-09 16:44:46 -0500

    Added crossOrigin tag to make sure dev servers can talk to each other

--- src/main/java/com/culinarycompanion/culinarycompanion/CulinaryCompanionController.java
+++ src/main/java/com/culinarycompanion/culinarycompanion/CulinaryCompanionController.java
@@ -1,14 +1,18 @@
 package com.culinarycompanion.culinarycompanion;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CulinaryCompanionController {
 
+    @CrossOrigin(origins = "http://localhost:5000")
     @RequestMapping("/recipes")
     public String index() {
         return "Fake recipes YEEEERRRRR";
     }
 
+    @RequestMapping("")
+    public String blank() { return "Culinary Companion base-page :)"; }
 }
