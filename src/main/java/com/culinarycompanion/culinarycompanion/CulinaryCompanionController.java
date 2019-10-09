cbabb4542f1831efac6f7c4b44f2319e218d7b0a
Grant <granttlalonde@gmail.com>
2019-10-09 17:35:06 -0500

    Created example Map to return for front-end use

--- src/main/java/com/culinarycompanion/culinarycompanion/CulinaryCompanionController.java
+++ src/main/java/com/culinarycompanion/culinarycompanion/CulinaryCompanionController.java
@@ -3,14 +3,18 @@ package com.culinarycompanion.culinarycompanion;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.*;
 
 @RestController
 public class CulinaryCompanionController {
 
     @CrossOrigin(origins = "http://localhost:5000")
     @RequestMapping("/recipes")
-    public String index() {
-        return "Fake recipes YEEEERRRRR";
+    public Map returnRecipes() {
+        Map potentialValues = new HashMap();
+        potentialValues.put("firstRecipe", "Chicken Tikka");
+        potentialValues.put("secondRecipe", "Steak au poivre");
+        return potentialValues;
     }
 
     @RequestMapping("")
